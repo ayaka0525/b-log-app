ed739de1746215bceb7ccdc2ba7111d7404aa7d7
eimii@0077 <yuimw03260323@yahoo.co.jp>
2024-08-21 19:10:17 +0900

    ダミーデータを利用

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController <  ApplicationController
+    #データの一覧を表示する
     def index
-        # Articleのデータベースにある最初のデータを取得。それをインスタンス編集(@article)に代入
-        @article = Article.first
+        # Articleのデータベースにある「すべて」のデータを取得。それをインスタンス編集(@article)に代入
+        @articles = Article.all
     end
 
     def about
