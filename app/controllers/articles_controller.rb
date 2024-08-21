f965be808a9ffdd14ec08e587f6ab94b40176523
eimii@0077 <yuimw03260323@yahoo.co.jp>
2024-08-21 19:41:08 +0900

    showを作成、link_toで囲ったエリアをリンクに変更。index action,  show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,9 @@ class ArticlesController <  ApplicationController
         @articles = Article.all
     end
 
-    def about
-
+    def show
+        #paramsのidとするのがrailsのルールのようなもの
+        @article = Article.find(params[:id])
     end
 
 end
