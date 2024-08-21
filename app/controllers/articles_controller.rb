d7b7a3922b41c27f4c569ac725193af025b2f0eb
eimii@0077 <yuimw03260323@yahoo.co.jp>
2024-08-21 23:01:28 +0900

    新規作成画面、データ保存してindexページに戻る

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,12 +10,26 @@ class ArticlesController <  ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+          redirect_to article_path(@article)
+        else
+          render :new
+        end
+      end
+
     def destroy
         article = Article.find(params[:id])
         article.destroy!
         redirect_to root_path, notice: '削除に成功しました'
 
     end
+
+    private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   root to: 'articles#index'
 
-  resources :articles, only:[:show, :destroy]
+  resources :articles, only:[:show, :new, :create, :destroy]
+
+  
 
 
 end
