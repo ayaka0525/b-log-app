b70dd2c8d9181e236a119756681f2cc576bf72dd
eimii@0077 <yuimw03260323@yahoo.co.jp>
2024-08-22 14:50:42 +0900

    作成日を挿入、article.rbにコードを書いて、index,showの該当する日付に挿入

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
 
+    def display_created_at
+        # 記事の作成日を挿入する。ja.ymlを引用している
+        I18n.l(self.created_at, format: :default)
+    end
+
 end
