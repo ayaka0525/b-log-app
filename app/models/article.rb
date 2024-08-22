092531d224a49beebc8047c3d8b3ed59537066a2
eimii@0077 <yuimw03260323@yahoo.co.jp>
2024-08-22 20:51:38 +0900

    Validationについて uniqueness 正規表現によるValidation 独自でValidationを定義する

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,15 +11,30 @@
 class Article < ApplicationRecord
     validates :title, presence: true
 
-    #lenght
+    #lenght(文字数制限)
     validates :title, length: { minimum:2, maximum: 100 }
+    #タイトルが@から始まらない
+    validates :title, format: { with: /\A(?!\@)/ }
+
     validates :content, length: { minimum:10 }
+    #同じ文章があると保存出来ないようにする
+    validates :content, uniqueness: true
 
     validates :content, presence: true
+
+    validate :validate_title_and_content_length
 
     def display_created_at
         # 記事の作成日を挿入する。ja.ymlを引用している
         I18n.l(self.created_at, format: :default)
     end
+
+    private
+
+    def validate_title_and_content_length
+        #タイトルと内容含めて100文字以内にする
+        char_count = self.title.length + self.content.length
+        errors.add(:content, '100文字以上で！') unless char_count > 100
+      end
 
 end
