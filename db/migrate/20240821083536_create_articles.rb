9ba0081ec71c71028592390220d181d10cd24348
eimii@0077 <yuimw03260323@yahoo.co.jp>
2024-08-24 15:28:26 +0900

    bd不要ファイルの削除、項目にユーザーを追加し、情報がないと登録できないような仕様に。表示もログイン状態じゃないとボタンが見れないように、author_nameアクション作成（ユーザー名表記）

--- db/migrate/20240821083536_create_articles.rb
+++ db/migrate/20240821083536_create_articles.rb
@@ -1,10 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
-    create_table :articles do |t|
+    change_table : articles do |t|
       #ここにカラム名(項目)入力していく
-      t.string:title
-      t.text:content
-
+      t.references :user, null: false
+      t.string:title, null: false
+      t.text:content, null: false
       t.timestamps
     end
   end
--- db/migrate/20240823073032_add_user_id_to_articles.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToArticles < ActiveRecord::Migration[6.0]
-  def change
-    #単純にIDを入れるだけではなく、検索・パフォーマンスをよくする目的
-    add_reference :articles, :user
-  end
-end
