d618e470dd4524394bf429f61f0ce020e13c38da
eimii@0077 <yuimw03260323@yahoo.co.jp>
2024-08-25 15:14:57 +0900

    フォーム編集をできるように、入力したら保存。コード改善

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,7 +7,37 @@ class ProfilesController < ApplicationController
     end
 
     def edit
-        #空の箱をつくる
-        @profile = current_user.build_profile
+        # if current_user.profile.present?
+        #     @profile = current_user.profile
+        # else
+        #     @profile = current_user.build_profile
+        #上のコードを短く出来る表現をつかう。
+        # @profile = current_user.prepare_profile
+
+
+        #空の箱をつくる,プロフィールが入っていたらその情報を引き継ぐ
+        @profile = current_user.prepare_profile
     end
+
+    def update
+        @profile = current_user.prepare_profile
+        @profile.assign_attributes(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィール更新！'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :birthday,
+      :subscribed
+    )
+  end
 end
