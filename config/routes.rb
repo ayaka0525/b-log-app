8e3af33f8bce513c15348441aa310fa50fb2e77e
eimii@0077 <yuimw03260323@yahoo.co.jp>
2024-09-18 17:17:12 +0900

    プロフィール機能の実装（フォロー、アンフォロー、プロフィールページ表示切り替え）

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,27 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'articles#index'
+    root to: 'articles#index'
 
-  #index以外のURLを使用しているので、onlyを外した。
-  resources :articles do
-  #記事の下にcomment URLが生成される。
-  resources :comments, only: [:new, :create]
-  #いいねできるように生成・削除でハート画像が変わる
-  resource :like, only: [:create, :destroy]
-  end
-  #プロフィールは１個なのでidを指定しなくてよく、urlにindex/:idがいらないから'resource'単数を使う。
-  resource :profile, only: [:show, :edit, :update]
+    #index以外のURLを使用しているので、onlyを外した。
+    resources :articles do
+      #記事の下にcomment URLが生成される。
+      resources :comments, only: [:new, :create]
+      #いいねできるように生成・削除でハート画像が変わる
+      resource :like, only: [:create, :destroy]
+    end
 
-  #いいねの数を取得するためのURL
-  resources :favorites, only: [:index]
+    #フォロー機能
+    resources :accounts, only: [:show] do
+      resources :follows, only: [:create]
+        resources :unfollows, only: [:create]
+    end
+
+    #プロフィールは１個なのでidを指定しなくてよく、urlにindex/:idがいらないから'resource'単数を使う。
+    resource :profile, only: [:show, :edit, :update]
+
+    #いいねの数を取得するためのURL
+    resources :favorites, only: [:index]
 
 end
